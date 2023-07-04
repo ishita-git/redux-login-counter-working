d614b1c2cd61fcf22beafb279847260e4260cc62
Ishita <iamod@tekion.com>
2023-07-04 17:19:51 +0530

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+
+import {  configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './counter.js'
+import authReducer from './auth.js'
+
+
+
+
+const store = configureStore({
+    reducer: {counter: counterReducer, auth: authReducer}
+});
+
+
+
+
+export default store;
